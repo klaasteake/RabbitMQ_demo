88a92f6a80be8f463d968b4137c912e25bd20530
Jeroen <Jeroen@JEROENDUEL>
2016-11-21 21:13:22 +0100

    Deelnemer formulier

--- Donald_Duck_FrontEnd/DeelnemerApp/src/app/app.component.ts
+++ Donald_Duck_FrontEnd/DeelnemerApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { IDeelnemer, FullnamePipe, DeelnemerService } from './deelnemer';
 
 @Component({
@@ -6,12 +7,32 @@ import { IDeelnemer, FullnamePipe, DeelnemerService } from './deelnemer';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
 
   deelnemers : IDeelnemer[];
+  registerForm : FormGroup;
 
-  constructor(private deelnemerService: DeelnemerService) {
+  constructor(private deelnemerService: DeelnemerService, private formBuilder: FormBuilder) {
     this.deelnemers = deelnemerService.getDeelnemers();
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+      bSN: ['', [Validators.required, Validators.pattern(/^[0-9]{8,9}$/)]],
+      birthDate: [new Date(), [Validators.required]]
+    });
+  }
+
+  register(model: IDeelnemer, isValid: boolean) {
+    // check if model is valid
+    // if valid, call API to save customer
+    console.log(model, isValid);
+    if(isValid){
+      this.deelnemers.push(model);
+      this.registerForm.reset();
+    }
   }
 }
--- Donald_Duck_FrontEnd/DeelnemerApp/src/app/app.module.ts
+++ Donald_Duck_FrontEnd/DeelnemerApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HttpModule } from '@angular/http';
 import { DeelnemerModule } from './deelnemer/deelnemer.module'
 import { DeelnemerService } from './deelnemer/deelnemer.service'
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     DeelnemerModule
   ],
